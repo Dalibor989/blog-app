46d4d0bd7222b05eaf2de29acb0d90fa2b3895c2
Dalibor989 <lazic_dalibor@hotmail.com>
2021-08-26 22:59:34 +0200

    finished single post implementation

--- src/pages/DetailedPost.js
+++ src/pages/DetailedPost.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import postService from "../services/PostService";
 
@@ -5,10 +6,27 @@ function DetailedPost() {
   const { id } = useParams();
   const post = postService.get(id);
 
+  const [newPost, setNewPost] = useState({
+    title: '',
+    text: '',
+  })
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const { id: _, ...restData } = await postService.get(id);
+
+      setNewPost({ ...restData });
+    };
+
+    if (id) {
+      fetchPost();
+    }
+  }, [id]);
+
   return (
     <div>
-      <h3>{post.title}</h3>
-      <p>{post.text}</p>
+      <h3>{newPost.title}</h3>
+      <p>{newPost.text}</p>
     </div>
   )
 }
